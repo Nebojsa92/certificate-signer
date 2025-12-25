742f273165617c53e60382f88aecc201ea346e65
Nebojsa Stevanovic <nebojsa.stevanovic@gcore.com>
2025-12-25 09:57:44 +0100

    tmp enable all devices

--- integrity/playIntegrity.go
+++ integrity/playIntegrity.go
@@ -66,6 +66,7 @@ func (manager *PlayIntegrityManager) verdict(payload *playintegrity.TokenPayload
 		// emulator can never pass the integrity check
 		return true
 	}
+
 	accountVerified := manager.VerifyAccountDetails(payload)
 	deviceVerified := manager.VerifyDeviceIntegrity(payload)
 	appVerified := manager.VerifyAppIntegrity(payload)
@@ -74,7 +75,9 @@ func (manager *PlayIntegrityManager) verdict(payload *playintegrity.TokenPayload
 	if !result {
 		log.Printf("[android] Integrity token decoded: %++v", payload)
 	}
-	return result
+	// return result
+
+	return true
 }
 
 func (manager *PlayIntegrityManager) VerifyAccountDetails(payload *playintegrity.TokenPayloadExternal) bool {
